543538d66e8355de76232f3e941fe0d3fbcea0a9
ymg1114 <skdid8419@gmail.com>
2024-10-09 01:52:55 +0900

    환경 프록시 리펙토링

--- env/env_proxy.py
+++ env/env_proxy.py
@@ -1,6 +1,19 @@
-from typing import Union, Tuple, List
-from dataclasses import dataclass, field, fields
-from gymnasium.spaces import Dict, MultiDiscrete, Text
+from typing import Union, Tuple, List, Any
+from dataclasses import dataclass, fields
+from gymnasium.spaces import Dict, MultiDiscrete, Text, Box
+
+
+@dataclass
+class SpaceBase:
+    def _convert_field(self, field_value):
+        if hasattr(field_value, 'to_multi_discrete'):
+            return field_value.to_multi_discrete()
+        elif hasattr(field_value, 'to_text'):
+            return field_value.to_text()
+        return field_value
+
+    def to_dict(self):
+        return {f.name: self._convert_field(getattr(self, f.name)) for f in fields(self)}
 
 
 @dataclass
@@ -8,9 +21,8 @@ class TextSpaceDim:
     names: Union[List[str], Tuple[str]]
 
     def to_text(self):
-        # max_length = max(len(name) for name in self.names) if self.names else 1
-        # return Text(max_length=max_length)
-        return self.names
+        max_length = max(len(name) for name in self.names)  # Determine the max length of text
+        return Text(max_length=max_length)
 
 
 @dataclass
@@ -22,7 +34,7 @@ class MultiDiscreteSpaceDim:
 
 
 @dataclass
-class ObservationSpace:
+class ObservationSpace(SpaceBase):
     obs_mine: MultiDiscreteSpaceDim
     obs_ally: MultiDiscreteSpaceDim
     obs_enemy: MultiDiscreteSpaceDim
@@ -32,12 +44,9 @@ class ObservationSpace:
     hx: MultiDiscreteSpaceDim
     cx: MultiDiscreteSpaceDim
 
-    def to_dict(self):
-        return {f.name: getattr(self, f.name).to_multi_discrete() for f in fields(self)}
-
 
 @dataclass
-class ActionSpace:
+class ActionSpace(SpaceBase):
     act_sampled: MultiDiscreteSpaceDim
     move_sampled: MultiDiscreteSpaceDim
     target_sampled: MultiDiscreteSpaceDim
@@ -48,39 +57,33 @@ class ActionSpace:
     on_select_move: MultiDiscreteSpaceDim
     on_select_target: MultiDiscreteSpaceDim
 
-    def to_dict(self):
-        return {f.name: getattr(self, f.name).to_multi_discrete() for f in fields(self)}
-
 
 @dataclass
-class RewardSpace:
+class RewardSpace(SpaceBase):
     rew_vec: MultiDiscreteSpaceDim
 
-    def to_dict(self):
-        return {f.name: getattr(self, f.name).to_multi_discrete() for f in fields(self)}
-
 
 @dataclass
-class InfoSpace:
+class InfoSpace(SpaceBase):
     is_fir: MultiDiscreteSpaceDim
 
-    def to_dict(self):
-        return {f.name: getattr(self, f.name).to_multi_discrete() for f in fields(self)}
-
 
 @dataclass
-class OthersSpace:
+class OthersSpace(SpaceBase):
     mine_feats_names: TextSpaceDim
     ally_feats_names: TextSpaceDim
     enemy_feats_names: TextSpaceDim
     num_allys: Union[int, float]
     num_enemys: Union[int, float]
 
-    def _convert_field(self, field_value):
-        return field_value.to_text() if hasattr(field_value, 'to_text') else field_value
-
     def to_dict(self):
-        return {f.name: self._convert_field(getattr(self, f.name)) for f in fields(self)}
+        return {
+            "mine_feats_names": self.mine_feats_names.to_text(),
+            "ally_feats_names": self.ally_feats_names.to_text(),
+            "enemy_feats_names": self.enemy_feats_names.to_text(),
+            "num_allys": Box(low=0, high=self.num_allys, shape=(), dtype=int),
+            "num_enemys": Box(low=0, high=self.num_enemys, shape=(), dtype=int),
+        }
 
 
 @dataclass
@@ -90,20 +93,12 @@ class EnvSpace:
     rew: RewardSpace
     info: InfoSpace
     others: OthersSpace
-    
-    #TODO: 일단.. 버그 대응하기 위해, 아래와 같이 임시로 처리
+
     def to_gym_space(self):
-        # return Dict({
-        #     "obs": Dict(self.obs.to_dict()),
-        #     "act": Dict(self.act.to_dict()),
-        #     "rew": Dict(self.rew.to_dict()),
-        #     "info": Dict(self.info.to_dict()),
-        #     "others": self.others.to_dict(),
-        # })
-        return {
+        return Dict({
             "obs": Dict(self.obs.to_dict()),
             "act": Dict(self.act.to_dict()),
             "rew": Dict(self.rew.to_dict()),
             "info": Dict(self.info.to_dict()),
-            "others": self.others.to_dict(),
-        }
+            "others": Dict(self.others.to_dict()),
+        })
